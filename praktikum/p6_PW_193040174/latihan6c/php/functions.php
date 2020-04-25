d9d4ca2ed61e6b96408f3326882e5882f7d385c4
ujangfatahak <63782886+ujangfatahak@users.noreply.github.com>
2020-04-26 05:59:53 +0700

    tambah data

---
+++ praktikum/p6_PW_193040174/latihan6c/php/functions.php
@@ -0,0 +1,54 @@
+<?php
+// function untuk melakukan koneksi ke database
+function koneksi()
+{
+  $conn = mysqli_connect("localhost", "root", "") or die("Koneksi ke DB Gagal");
+  mysqli_select_db($conn, "tubes_193040174") or die("Database salah!");
+
+  return $conn;
+}
+
+// function untuk melakukan query ke database
+function query($sql)
+{
+  $conn = koneksi();
+  $result = mysqli_query($conn, "$sql");
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  return $rows;
+}
+
+function tambah($data)
+{
+  $conn = koneksi();
+
+  $gambar = htmlspecialchars($data['gambar']);
+  $nama = htmlspecialchars($data['nama']);
+  $deskripsi = htmlspecialchars($data['deskripsi']);
+  $asaldaerah = htmlspecialchars($data['asal_daerah']);
+  $carapemakaian = htmlspecialchars($data['cara_pemakaian']);
+  $harga = htmlspecialchars($data['harga']);
+
+
+  $query = "INSERT INTO
+              alat musik
+            VALUES
+            ('', '$gambar', '$nama', '$deskripsi', '$asaldaerah', '$carapemakaian', '$harga');
+  
+  ";
+  mysqli_query($conn, $query);
+  return mysqli_affected_rows($conn);
+}
+
+function hapus($id)
+{
+  $conn = koneksi();
+
+  mysqli_query($conn, "DELETE FROM alat musik WHERE id = $id");
+
+  return mysqli_affected_rows($conn);
+}
---
+++ praktikum/p6_PW_193040174/latihan6e/index.php
@@ -0,0 +1,32 @@
+<?php
+// menghubungkan dengan file php lainnya
+require 'php/functions.php';
+
+// melakukan query
+$alatmusik = query("SELECT * FROM alat musik");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <div class="container">
+    <h1 align="center">Alat Musik</h3>
+      <?php foreach ($alatmusik as $am) : ?>
+        <p class="nama">
+          <a href="php/detail.php?id=<?= $am['id'] ?>">
+            <?= $am['nama'] ?>
+          </a>
+        </p>
+      <?php endforeach; ?>
+  </div>
+</body>
+
+</html>
